16f861087e80028168e251165be4b2e0ec179cbc
VoThiKimHue <vothikimhue9a1819.@gmail.com>
2024-03-28 12:34:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// const Login = () => {
+//     if (checkEmailTT() == 1 && checkSyntax() == 1) {
+//       getInfo();
+//       window.location.href =
+//         "http://127.0.0.1:5500/task.html";
+//     }
+//   };
+const Login = () => {
+    window.location.href =
+      "http://127.0.0.1:5500/task.html";
+  };
