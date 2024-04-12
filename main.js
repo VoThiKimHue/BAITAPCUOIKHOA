e8b0a632b32036f38cfda345968f524ffb5e2cee
VoThiKimHue <vothikimhue9a1819.@gmail.com>
2024-04-12 15:03:45 +0700

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-// const Login = () => {
-//     if (checkEmailTT() == 1 && checkSyntax() == 1) {
-//       getInfo();
-//       window.location.href =
-//         "http://127.0.0.1:5500/task.html";
-//     }
-//   };
+document.getElementById("login-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var Email = document.getElementById("Email").value;
+  var Password = document.getElementById("Password").value;
+
+  // Kiểm tra định dạng email
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(Email)) {
+      document.getElementById("error-msg").textContent = "Invalid email format";
+      return;
+  }
+
+  // Chuyển hướng sau khi đăng nhập thành công
+  window.location.href = "http://127.0.0.1:5500/todoList.html";
+});
 const Login = () => {
-    window.location.href =
-      "http://127.0.0.1:5500/task.html";
-  };
+       window.location.href =     
+             "http://127.0.0.1:5500/todoList.html";     
+     };
